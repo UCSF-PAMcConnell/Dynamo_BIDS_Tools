af8f4bea7ae6fc01584a26176e7512e56dfe3d89
Patrick A. McConnell <mcconnep@musc.edu>
2023-10-20 10:50:56 -0400

    initial commit
    code derived from pydicom

---
+++ sort_dicom_files.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import pydicom
+import sys
+
+def sort_dicom_files(input_directory, output_directory):
+    # Ensure output directory exists
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Loop through each entity in the input directory
+    for filename in os.listdir(input_directory):
+        filepath = os.path.join(input_directory, filename)
+        
+        # Check if the entity is a file
+        if os.path.isfile(filepath):
+            # Try to read the DICOM file
+            try:
+                dicom_file = pydicom.dcmread(filepath)
+                
+                # Extract sequence information (you might need to adjust this based on your specific DICOM files)
+                # sequence_name = dicom_file.SequenceName  # Adjust attribute if necessary
+                sequence_name = dicom_file.SeriesDescription  # Adjust attribute if necessary
+
+                # Create a directory for the sequence if it doesn't exist
+                sequence_directory = os.path.join(output_directory, sequence_name)
+                if not os.path.exists(sequence_directory):
+                    os.makedirs(sequence_directory)
+                
+                # Move the DICOM file to the corresponding sequence folder
+                shutil.move(filepath, os.path.join(sequence_directory, filename))
+                
+            except pydicom.errors.InvalidDicomError:
+                print(f"Warning: {filename} is not a valid DICOM file and will be skipped.")
+            except Exception as e:
+                print(f"Could not process file {filename}: {e}")
+
+# Check if the right number of command-line arguments are provided
+if len(sys.argv) != 3:
+    print("Usage: python sort_dicom_files.py <input_directory> <output_directory>")
+    sys.exit(1)
+
+# Get the paths from the command-line arguments
+input_directory = sys.argv[1]
+output_directory = sys.argv[2]
+
+# Call the function with the provided arguments
+sort_dicom_files(input_directory, output_directory)
