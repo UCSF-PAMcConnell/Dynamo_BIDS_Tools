eb1c100f44a8859a1690a741f34ee6e39b81c24c
Patrick A. McConnell <mcconnep@musc.edu>
2023-10-24 14:23:44 -0400

    updated
    updated to run from dicom_root_dir and included IntendedFor and B0FieldIdentifier

--- process_fmap_gre.py
+++ process_fmap_gre.py
@@ -7,7 +7,7 @@ import json
 import shutil
 import tempfile
 
-# input arguments: <dicom_dir> <bids_root>
+# input arguments: <dicom_root_dir> <bids_root>
 # Requires definition of "IntendedFor" task volumes
 #{
 #   "IntendedFor": [
@@ -16,7 +16,7 @@ import tempfile
 #    ]
 #}
 
-def update_json_file(json_filepath):
+def update_json_file(json_filepath, intended_for=None):
     """
     Updates specific fields in a JSON file.
     
@@ -27,11 +27,13 @@ def update_json_file(json_filepath):
         data = json.load(file)
         data['EchoTime1'] = 0.00492
         data['EchoTime2'] = 0.00738
+        data['B0FieldIdentifier'] = "*fm2d2r"
+        data['IntendedFor'] = intended_for
         file.seek(0)
         json.dump(data, file, indent=4)
         file.truncate()
 
-def extract_ids(dicom_dir):
+def extract_ids(dicom_root_dir):
     """
     Extracts subject and session IDs from the DICOM directory path.
     
@@ -41,8 +43,8 @@ def extract_ids(dicom_dir):
     Returns:
     tuple: Subject ID and session ID as strings.
     """
-    match_subject = re.search(r'sub-(\w+)', dicom_dir)
-    match_session = re.search(r'ses-(\w+)', dicom_dir)
+    match_subject = re.search(r'sub-(\w+)', dicom_root_dir)
+    match_session = re.search(r'ses-(\w+)', dicom_root_dir)
     
     if match_subject and match_session:
         return match_subject.group(1), match_session.group(1)
@@ -71,16 +73,19 @@ def run_dcm2niix(input_dir, output_dir_temp):
 # Main code execution starts here when the script is run
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process DICOM files and convert to NIfTI.')
-    parser.add_argument('dicom_dir', type=str, help='Directory containing the DICOM files.')
+    parser.add_argument('dicom_root_dir', type=str, help='Root directory containing the DICOM directories.')
     parser.add_argument('bids_root', type=str, help='Root directory of the BIDS dataset.')
     
     args = parser.parse_args()
     
-    subject_id, session_id = extract_ids(args.dicom_dir)
+    # Specify the exact directory where the DICOM files are located within the root directory
+    dicom_dir = os.path.join(args.dicom_root_dir, 'fmap_gre_siemens')
+
+    subject_id, session_id = extract_ids(args.dicom_root_dir)
     
     # Create a temporary directory to store the dcm2niix output
     with tempfile.TemporaryDirectory() as tmpdirname:
-        run_dcm2niix(args.dicom_dir, tmpdirname)
+        run_dcm2niix(dicom_dir, tmpdirname)
         
         # Define the BIDS 'fmap' directory where the files should ultimately be saved
         fmap_dir_bids = os.path.join(args.bids_root, f'sub-{subject_id}', f'ses-{session_id}', 'fmap')
@@ -106,4 +111,12 @@ if __name__ == "__main__":
                     shutil.copy2(old_file, new_file_path)
                     
                     if old_file.endswith('.json'):
-                        update_json_file(new_file_path)
+                        intended_for = [f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-01_bold.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-02_bold.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-03_bold.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-04_bold.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-01_sbref.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-02_sbref.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-03_sbref.nii",
+                        f"ses-{session_id}/func/sub-{subject_id}_ses-{session_id}_task-rest_run-04_sbref.nii"]
+                        update_json_file(new_file_path, intended_for)
