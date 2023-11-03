f198738f71fd0d4a5d8080d11592e26afba09aa9
Patrick A. McConnell <mcconnep@musc.edu>
2023-11-03 10:35:14 -0400

    tested and working, configuring ppg add on

--- process_physio.py
+++ process_physio.py
@@ -38,7 +38,7 @@ def segment_data_into_runs(data, trigger_starts, tr, sampling_rate, num_volumes_
         actual_interval = trigger_starts[i + num_volumes_per_run - 1] - trigger_starts[i]
         
         # Debugging: Log the checking process
-        print(f"Checking triggers from index {i}: expected interval {expected_interval}, actual interval {actual_interval}")
+        #print(f"Checking triggers from index {i}: expected interval {expected_interval}, actual interval {actual_interval}")
         
         if actual_interval <= expected_interval:
             start_idx = trigger_starts[i]
@@ -65,10 +65,12 @@ def save_segments_to_tsv_and_json(segments, bids_root_dir, subject_id, session_i
          'EDA - EDA100C-MRI': 'eda',
          'MRI Trigger - Custom, HLT100C - A 4': 'trigger'
      }
-
+    # Check if 'PPG - PPG100C' is in the labels and add it to the dictionary
+    if 'PPG - PPG100C' in original_labels:
+        bids_labels['PPG - PPG100C'] = 'ppg'
+   
     columns_to_keep = [idx for idx, label in enumerate(original_labels) if label in bids_labels]
     new_labels = [bids_labels[original_labels[idx]] for idx in columns_to_keep]
-
     segment_data = segments[0]['data']
     filtered_segment_data = segment_data[:, columns_to_keep]
     df_segment = pd.DataFrame(filtered_segment_data, columns=new_labels)
@@ -76,26 +78,6 @@ def save_segments_to_tsv_and_json(segments, bids_root_dir, subject_id, session_i
     os.makedirs(output_dir, exist_ok=True)
     filename = f"sub-{subject_id}_ses-{session_id}_task-rest_run-{str(run_index).zfill(2)}_physio.tsv.gz"
     filepath = os.path.join(output_dir, filename)
-
-    # Create metadata dictionary
-    metadata = {
-        "SamplingFrequency": sampling_rate,
-        "StartTime": segments[0]['start_index'] / sampling_rate,
-        "Columns": ["cardiac", "respiratory", "eda"],
-        "Manufacturer": "Biopac",
-        "cardiac": {
-            "Description": "continuous ecg measurement",
-            "Units": "mV"
-        },
-        "respiratory": {
-            "Description": "continuous measurements by respiration belt",
-            "Units": "mV"
-        },
-        "eda": {
-            "Description": "continuous measurements by foot electrodermal",
-            "Units": "microsiemens"
-        }
-    }
     with gzip.open(filepath, 'wt') as f_out:
         df_segment.to_csv(f_out, sep='\t', index=False)
     print(f"Saved segment for run {run_index} to {output_dir} with filename {filename}")
@@ -127,7 +109,7 @@ def plot_full_data_with_segments(data, runs, sampling_rate, original_labels, out
     plt.savefig(output_fig_path, dpi=300)
     print(f"Saved plot to {output_fig_path}")
 
-def main(physio_root_dir, bids_root_dir):
+def main(physio_root_dir, bids_root_dir, filtered_labels):
     """Process and convert physiological data to BIDS format."""
     pattern = r'sub-(?P<subject_id>\w+)/ses-(?P<session_id>\w+)/physio'
     match = re.search(pattern, physio_root_dir)
@@ -136,7 +118,6 @@ def main(physio_root_dir, bids_root_dir):
     subject_id = match.group('subject_id')
     session_id = match.group('session_id')
     print(f"Processing subject: {subject_id}, session: {session_id}")
-
     mat_file_name = f"sub-{subject_id}_ses-{session_id}_task-rest_physio.mat"
     mat_file_path = os.path.join(physio_root_dir, mat_file_name)
     print(f"Loading data from {mat_file_path}")
@@ -160,54 +141,100 @@ def main(physio_root_dir, bids_root_dir):
     start_from_trigger_index = 0
 
     while True:
-        # Debugging: Print the index from where the next segment search will start
         print(f"Searching for runs starting from trigger index {start_from_trigger_index}")
-
         json_file_name = f"sub-{subject_id}_ses-{session_id}_task-rest_run-{str(run_idx).zfill(2)}_bold.json"
         json_file_path = os.path.join(bids_root_dir, f"sub-{subject_id}", f"ses-{session_id}", 'func', json_file_name)
-        with open(json_filepath, 'w') as f_json:
-            json.dump(metadata, f_json, indent=4)
-       
-        print(f"Saved JSON metadata for run {run_index} to {output_dir} with filename {json_file_name}")
-
-        # If the JSON file does not exist, break out of the loop
         if not os.path.exists(json_file_path):
             print(f"No JSON file found for run {str(run_idx).zfill(2)}. Ending search for runs.")
             break
-
-        # Debugging: Print the current run being processed
-        print(f"Processing subject: sub-{subject_id}, ses-{session_id}, run-{str(run_idx).zfill(2)}")
+        print(f"Processing run: sub-{subject_id}, ses-{session_id}, run-{str(run_idx).zfill(2)}")
         json_metadata = load_json_metadata(json_file_path)
         num_volumes_per_run = json_metadata.get('NumVolumes')
         tr = json_metadata.get('RepetitionTime')
-
         if num_volumes_per_run is None or tr is None:
             raise ValueError(f"JSON metadata for run {run_idx} does not contain 'NumVolumes' or 'RepetitionTime'.")
-
-        # Process each run individually
         runs, next_start_index = segment_data_into_runs(data, trigger_starts, tr, sampling_rate, num_volumes_per_run, start_from_trigger_index)
-
-        # Check if the segments for the current run are valid and non-empty
         if runs:
             current_run_segment = runs[0]
             all_runs.append(current_run_segment)
-            save_segments_to_tsv_and_json([current_run_segment], bids_root_dir, subject_id, session_id, labels, run_idx)
+
+            # Save the segment to TSV and then save the corresponding JSON metadata
+            save_segments_to_tsv_and_json([current_run_segment], bids_root_dir, subject_id, session_id, filtered_labels, run_idx, sampling_rate)
+
+            # Save the corresponding JSON metadata for the physio
+            physio_json_metadata = {
+                "SamplingFrequency": {
+                    "Value": sampling_rate,
+                    "Units": "Hz",
+                },
+                "StartTime": {
+                    "Value": current_run_segment['start_index'] / sampling_rate,
+                    "Description": "start time of current run relative to recording onset",
+                    "Units": "seconds",
+                },
+                "Columns": filtered_labels,
+                "Manufacturer": "Biopac",
+                "cardiac": {
+                    "Description": "continuous ecg measurement",
+                    "Placement": "Lead 1",
+                    "Units": "mV",
+                    "Gain": 500,
+                    "35HzLPN": "off / 150HzLP",
+                    "HPF": "0.05 Hz",
+                }
+                if 'cardiac' in filtered_labels
+                else None,
+                "respiratory": {
+                    "Description": "continuous measurements by respiration belt",
+                    "Units": "Volts",
+                    "Gain": 10,
+                    "LPF": "10 Hz",
+                    "HPF1": "DC",
+                    "HPF2": "0.05 Hz",
+                }
+                if 'respiratory' in filtered_labels
+                else None,
+                "eda": {
+                    "Description": "continuous eda measurement",
+                    "Placement": "right plantar instep",
+                    "Units": "microsiemens",
+                    "Gain": 5,
+                    "LPF": "1.0 Hz",
+                    "HPF1": "DC",
+                    "HPF2": "DC",
+                }
+                if 'eda' in filtered_labels
+                else None,
+                "ppg": {
+                    "Description": "continuous ppg measurement",
+                    "Placement": "left index toe",
+                    "Units": "Volts",
+                    "Gain": 10,
+                    "LPF": "3.0 Hz",
+                    "HPF1": "0.5 Hz",
+                    "HPF2": "0.05 Hz",
+                }
+                if 'ppg' in filtered_labels
+                else None,
+            }
+            physio_json_file_name = f"sub-{subject_id}_ses-{session_id}_task-rest_run-{str(run_idx).zfill(2)}_physio.json"
+            physio_json_file_path = os.path.join(bids_root_dir, f"sub-{subject_id}", f"ses-{session_id}", 'func', physio_json_file_name)
+            with open(physio_json_file_path, 'w') as f_json:
+                json.dump(physio_json_metadata, f_json, indent=4)
+            print(f"Saved physio JSON metadata for run {run_idx} to {physio_json_file_path}")
             start_from_trigger_index = next_start_index
         else:
             print(f"No valid segments found for run {str(run_idx).zfill(2)}. Ending search for runs.")
             break
-
         run_idx += 1  # Increment run index for next iteration
-
-    # Plot the full data with all run segments highlighted after processing all runs
     output_fig_path = os.path.join(physio_root_dir, f"{mat_file_name.replace('.mat', '')}_all_runs.png")
     plot_full_data_with_segments(data, all_runs, sampling_rate, labels, output_fig_path)
-
     print("Processing complete.")
 
+# Main code execution
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert physiological data to BIDS format.")
     parser.add_argument("physio_root_dir", help="Directory containing the physiological .mat file.")
     parser.add_argument("bids_root_dir", help="Path to the root of the BIDS dataset.")
     args = parser.parse_args()
-    main(args.physio_root_dir, args.bids_root_dir)
+    main(args.physio_root_dir, args.bids_root_dir, filtered_labels=['cardiac', 'respiratory', 'eda', 'ppg'])
