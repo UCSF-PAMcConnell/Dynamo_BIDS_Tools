27d3e8503a89895118e28799367f960e083938be
Patrick A. McConnell <mcconnep@musc.edu>
2023-11-04 09:30:55 -0400

    updated def main and function call

--- process_physio_ses_1.py
+++ process_physio_ses_1.py
@@ -19,69 +19,114 @@ logging.basicConfig(
 # Helper functions
 
 def parse_arguments():
-    # Parse command line arguments
-    # ...
+    # Parses command line arguments for the physio and BIDS root directories
+    parser = argparse.ArgumentParser(description='Process physiological data into BIDS format.')
+    parser.add_argument('physio_root_dir', type=str, help='Root directory of the physiological data.')
+    parser.add_argument('bids_root_dir', type=str, help='Root directory of the BIDS data.')
+    return parser.parse_args()
+
 
 def load_mat_file(mat_file_path):
-    # Load .mat file and return contents
-    # ...
+    # Loads the .mat file and extracts labels, data, and units
+    logging.info(f"Loading MAT file from {mat_file_path}")
+    mat_contents = sio.loadmat(mat_file_path)
+    labels = mat_contents['labels']
+    data = mat_contents['data']
+    units = mat_contents['units']
+    return labels, data, units
+
 
 def find_runs(triggers, num_volumes):
-    # Identify runs based on triggers and number of volumes
-    # ...
+    # Identifies the start and end indices of each run based on MR triggers and number of volumes
+    logging.info("Identifying runs based on triggers and number of volumes")
+    runs = []
+    # Logic to identify runs...
+    return runs
+
 
 def segment_data(data, runs, sampling_rate):
-    # Segment data based on identified runs
-    # ...
+    # Segments the physiological data based on the identified runs
+    logging.info("Segmenting data into runs")
+    segmented_data = []
+    # Logic to segment data...
+    return segmented_data
+
 
 def rename_channels(labels):
-    # Rename channels according to BIDS convention
-    # ...
+    # Renames channels according to BIDS convention
+    logging.info("Renaming channels according to BIDS conventions")
+    bids_labels = {}
+    # Logic to rename labels...
+    return bids_labels
+
 
 def write_output_files(segmented_data, metadata, output_dir):
-    # Write the output .tsv.gz and .json files
-    # ...
+    # Writes the output .tsv.gz and .json files for each run
+    logging.info(f"Writing output files to {output_dir}")
+    # Logic to write files...
+
 
 def plot_runs(data, runs, output_file):
-    # Plot the physiological data for all runs
-    # ...
+    # Plots the physiological data for all runs and saves the figure
+    logging.info(f"Plotting runs and saving to {output_file}")
+    # Logic to plot data...
+    plt.savefig(output_file)
+
 
 def extract_metadata_from_json(json_file_path):
-    # Extract metadata from .json file
-    # ...
+    # Extracts necessary metadata from the .json file associated with each run
+    logging.info(f"Extracting metadata from {json_file_path}")
+    with open(json_file_path, 'r') as file:
+        metadata = json.load(file)
+    return metadata
 
-def main():
-    # Main function
+
+# Main function to orchestrate the conversion process
+def main(physio_root_dir, bids_root_dir):
+    # Main logic here
     try:
-        # Parse input arguments
-        args = parse_arguments()
 
-        # Determine file paths and subject/session info
-        mat_file_path, subject_id, session_id = ...
+        # Extract subject_id and session_id from the physio_root_dir path
+        # Assuming the path follows the pattern: .../sub-<subject_id>_ses-<session_id>_...
+
+        subject_id = os.path.basename(physio_root_dir).split('_')[0]
+        session_id = os.path.basename(physio_root_dir).split('_')[1]
+        
+        # Construct the path to the .mat file using the naming convention
+        mat_file_name = f"{subject_id}_{session_id}_task-rest_physio.mat"
+        mat_file_path = os.path.join(physio_root_dir, mat_file_name)
 
         # Load .mat file
-        mat_contents = load_mat_file(mat_file_path)
-
-        # Extract information from .mat file
-        labels, units, data = ...
-
-        # Rename channels according to BIDS convention
+        labels, data, units = load_mat_file(mat_file_path)
+        
+        # Rename channels based on dynamic labels from data
         bids_labels = rename_channels(labels)
-
-        # Extract run metadata from BIDS .json files
-        run_metadata = ...
-
-        # Identify and segment runs
-        runs = find_runs(...)
-        segmented_data = segment_data(...)
-
-        # Write output files
-        write_output_files(segmented_data, bids_labels, ...)
-
-        # Plot runs
-        plot_file = ...
+        
+        # Loop through each run directory in BIDS format
+        # Iterate over the runs
+        for run_idx in range(1, 5):  # Assuming there are 4 runs as specified
+            run_id = f"run-{run_idx:02d}"
+            
+            # Construct the path to the run's .json file
+            json_file_name = f"{subject_id}_{session_id}_task-rest_{run_id}_bold.json"
+            json_file_path = os.path.join(bids_root_dir, subject_id, session_id, 'func', json_file_name)
+            
+            # Extract metadata from the run's .json file
+            run_metadata = extract_metadata_from_json(json_file_path)
+            
+            # Find the runs in the data
+            runs = find_runs(data, run_metadata['NumVolumes'])
+            
+            # Segment the data based on the runs
+            segmented_data = segment_data(data, runs, run_metadata['SamplingFrequency'])
+            
+            # Write the output files for this run
+            output_dir = os.path.join(physio_root_dir, subject_id, session_id, 'func')
+            write_output_files(segmented_data, run_metadata, output_dir)
+        # After processing all runs, plot the physiological data to verify alignment
+        plot_file = f"{physio_root_dir}/{subject_id}_{session_id}_task-rest_all_runs_physio.png"
         plot_runs(data, runs, plot_file)
-
+        
         logging.info("Process completed successfully.")
 
     except Exception as e:
@@ -89,4 +134,8 @@ def main():
         raise
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Convert physiological data to BIDS format.")
+    parser.add_argument("physio_root_dir", help="Directory containing the physiological .mat file.")
+    parser.add_argument("bids_root_dir", help="Path to the root of the BIDS dataset.")
+    args = parser.parse_args()
+    main(args.physio_root_dir, args.bids_root_dir)
