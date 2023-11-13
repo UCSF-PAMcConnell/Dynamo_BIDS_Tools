e79f660627c159e4324ef55c51657605a8f88dcb
Patrick A. McConnell <mcconnep@musc.edu>
2023-11-12 21:22:27 -0500

    testing

--- process_task_learn_beh_to_BIDS.py
+++ process_task_learn_beh_to_BIDS.py
@@ -1,27 +1,105 @@
-import scipy.io
-import pandas as pd
-import argparse
-import os
-import glob
-import logging
-import re
-import json
-import sys
+"""
+process_task_learn_beh_to_BIDS.py
 
-# Configuring logging to display informational messages and errors
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+Description:
+This script processes behavioral data for Task fMRI studies from MATLAB files and converts them into BIDS-compliant TSV files. 
+It loads MATLAB files, extracts necessary task event information, and formats the data into a structured DataFrame suitable for BIDS. 
+Additionally, it generates accompanying JSON sidecar files with metadata. The script features robust error handling, logging, and is 
+designed for easy integration into BIDS data processing pipelines.
 
-# Returns the name of the current conda environment.
-def get_conda_env():
-    try:
-        env_name = os.environ.get('CONDA_DEFAULT_ENV')
-        if env_name:
-            return env_name
-        else:
-            return None
-    except Exception as e:
-        logging.error(f"Error getting Conda environment: {e}")
-        return None
+Usage:
+Invoke the script from the command line with the following format:
+python process_task_learn_beh_to_BIDS.py <matlab_dir> <bids_root_dir>
+
+Example usage:
+python process_task_learn_beh_to_BIDS.py /path/to/matlab_dir /path/to/bids_root_dir
+
+Author: PAMcConnell
+Created on: 20231112
+Last Modified: 20231112
+Version: 1.0.0
+
+License:
+This software is released under the MIT License.
+
+Dependencies:
+- Python 3.12
+- Python libraries: scipy, pandas, argparse, os, glob, logging, re, json, sys.
+- MATLAB files containing task fMRI behavioral data.
+
+Environment Setup:
+- Ensure Python 3.12 is installed in your environment.
+- Install required Python libraries using 'pip install scipy pandas'.
+- Ensure that MATLAB files are structured correctly and accessible in the specified directory.
+
+Change Log:
+- 20231111: Initial release of the script with basic functionality for loading and processing MATLAB files.
+"""
+
+import scipy.io                   # for loading .mat files
+import pandas as pd               # for loading .csv files
+import argparse                   # for command line arguments
+import os                         # for file and directory operations
+import glob                       # for unix file pattern matching
+import logging                    # for logging
+import re                         # for regular expressions
+import json                       # for json file operations
+import sys                        # for sys.exit and sys.argv
+
+# Set up logging for individual archive logs.
+def setup_logging(subject_id, session_id, bids_root_dir):
+    """
+    Sets up logging for the script, creating log files in a specified directory.
+
+    Parameters:
+    - subject_id (str): The identifier for the subject.
+    - session_id (str): The identifier for the session.
+    - bids_root_dir (str): The root directory of the BIDS dataset.
+
+    This function sets up a logging system that writes logs to both a file and the console. 
+    The log file is named based on the subject ID, session ID, and the script name. 
+    It's stored in a 'logs' directory within the 'doc' folder by subject ID, which is located at the same 
+    level as the BIDS root directory.
+
+    The logging level is set to INFO, meaning it captures all informational, warning, and error messages.
+
+    Usage Example:
+    setup_logging('sub-01', 'ses-1', '/path/to/bids_root_dir')
+    """
+
+    # Extract the base name of the script without the .py extension.
+    script_name = os.path.basename(__file__).replace('.py', '')
+
+    # Construct the log directory path within 'doc/logs'
+    log_dir = os.path.join(os.path.dirname(bids_root_dir), 'doc', 'logs', script_name, subject_id)
+
+    # Create the log directory if it doesn't exist.
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Construct the log file name using subject ID, session ID, and script name.
+    log_file_name = f"{subject_id}_{session_id}_{script_name}.log"
+    log_file_path = os.path.join(log_dir, log_file_name)
+
+    # Configure file logging.
+    logging.basicConfig(
+        level=logging.INFO,
+        # filename='process_physio_ses_2.log', # Uncomment this line to save log in script execution folder.
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        filename=log_file_path,
+        filemode='w' # 'w' mode overwrites existing log file.
+    )
+
+    # If you also want to log to console.
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(console_handler)
+
+    logging.info(f"Logging setup complete. Log file: {log_file_path}")
+
+    return log_file_path
 
 # Load MATLAB data from the provided file path and extract relevant fields.
 def load_matlab_data(matlab_file_path):
@@ -125,28 +203,13 @@ def save_as_tsv(data, output_path):
         except Exception as e:
             logging.error(f"Error saving data to {output_path}: {e}")
 
-# Main function
-if __name__ == "__main__":
-    
-    # Desired Conda environment name
-    desired_env = "fmri"
-
-    # Check if the correct Conda environment is activated
-    current_env = get_conda_env()
-    if current_env != desired_env:
-        logging.error(f"Script is not running in the '{desired_env}' Conda environment. Current environment: '{current_env}'")
-        logging.info(f"Please activate the correct Conda environment by running: conda activate {desired_env}")
-        sys.exit(1)
-    else:
-        logging.info(f"Running in the correct Conda environment: '{current_env}'")
-
-    # Set up argument parser
-    parser = argparse.ArgumentParser(description='Process behavioral files and convert to .tsv in BIDS format.')
-    parser.add_argument('matlab_dir', type=str, help='Directory containing the MATLAB files.')
-    parser.add_argument('bids_root_dir', type=str, help='Root directory of the BIDS dataset where .tsv files will be saved.')
-    args = parser.parse_args()
-    
-    # Define the order in which runs should appear
+def main(matlab_root_dir, bids_root_dir):
+    """
+    Parameters:
+    - matlab_root_dir (str): Directory containing the MATLAB files.
+    - bids_root_dir (str): Root directory of the BIDS dataset where .tsv files will be saved.
+    """
+   # Define the order in which runs should appear
     run_order = [
         ("localizer_run1", "run-00"),
         ("localizer_run2", "run-07")
@@ -154,9 +217,9 @@ if __name__ == "__main__":
 
     # Defining specific run_ids for localizer runs
     localizer_run_ids = ["run-00", "run-07"]
-    
+
     # Get all MATLAB files from the specified directory
-    matlab_files = glob.glob(os.path.join(args.matlab_dir, "LRN*_*.mat"))
+    matlab_files = glob.glob(os.path.join(args.matlab_root_dir, "LRN*_*.mat"))
 
     for matlab_file_path in sorted(matlab_files):  # Processing files in alphabetical order
         try:
@@ -188,10 +251,30 @@ if __name__ == "__main__":
             filename_parts = filename.split('_')
             subject_id = filename_parts[0]
             output_path = os.path.join(args.bids_root_dir, f"sub-{subject_id}", 'ses-2', 'func',
-                                       f"sub-{subject_id}_ses-2_task-learn_{run_id}_events.tsv")
+                                        f"sub-{subject_id}_ses-2_task-learn_{run_id}_events.tsv")
             
             # Save formatted data as .tsv with .json sidecar
             save_as_tsv(formatted_data, output_path)
             logging.info(f"Processed {matlab_file_path} and saved .tsv file to: {output_path}")
         except Exception as e:
             logging.error(f"An error occurred while processing {matlab_file_path}: {e}")
+
+# Main function executed when the script is run from the command line.
+if __name__ == "__main__":
+    
+   # Set up an argument parser to handle command-line arguments.
+    parser = argparse.ArgumentParser(description='Process .mat behavioral files for TASK FMRI and convert to BIDS tsv.')
+
+    # Add arguments to the parser.
+
+    # The first argument is the root directory containing the DICOM directories.
+    parser.add_argument('matlab_root_dir', type=str, help='Root directory containing the DICOM directories.')
+    
+    # The second argument is the root directory of the BIDS dataset.
+    parser.add_argument('bids_root_dir', type=str, help='Root directory of the BIDS dataset.')
+
+    # Parse the arguments provided by the user.
+    args = parser.parse_args()
+
+    # Call the main function with the parsed arguments.
+    main(args.dicom_root_dir, args.bids_root_dir)
